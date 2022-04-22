429f31859d42f9b8685c80934486efb711b20f37
lijian <104197995+lijian9553@users.noreply.github.com>
2022-04-22 21:41:14 +0800

    Delete springboot-jpa directory
    delete springboot-jpa

--- springboot-jpa/src/main/java/com/example/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.repository;
-
-import com.example.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface UserRepository extends JpaRepository<User,Integer>, JpaSpecificationExecutor<User> {
-}
